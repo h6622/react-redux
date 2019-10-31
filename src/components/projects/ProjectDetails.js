66e1d0b1c400d95ada48053d91eb81479c9c7be0
sh183694@gmail.com <sh183694@gamil.com>
2019-10-31 17:13:55 +0900

    Create ProjectDetail, Summary

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function ProjectDetails() {
+  return (
+    <div className=".container section project-details">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Project Title</span>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam
+            nobis quasi quo placeat error corporis cum! Iste magnam asperiores,
+            molestias atque nulla impedit et doloribus esse quidem, reiciendis
+            repellendus debitis!
+          </p>
+        </div>
+        <div className="card-action grey lighten-4 grey-text">
+          <div>Posted by The h662</div>
+          <div>2nd September, 2am</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectDetails;
