a91f70fdfc5a173a296917082f6342d87605fa40
sh183694@gmail.com <sh183694@gamil.com>
2019-11-01 00:32:40 +0900

    CREATE reducers

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./index.css";
+import { createStore } from "redux";
+import rootReducer from "./store/reducers/rootReducer";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
