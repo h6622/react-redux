e23103e675a4cd0246bd1de17ba3b57054ba4da2
sh183694@gmail.com <sh183694@gamil.com>
2019-10-31 17:23:52 +0900

    add projectdetails id

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-function ProjectDetails() {
+function ProjectDetails(props) {
+  const id = props.match.params.id;
   return (
     <div className=".container section project-details">
       <div className="card z-depth-0">
         <div className="card-content">
-          <span className="card-title">Project Title</span>
+          <span className="card-title">Project Title - {id}</span>
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam
             nobis quasi quo placeat error corporis cum! Iste magnam asperiores,
